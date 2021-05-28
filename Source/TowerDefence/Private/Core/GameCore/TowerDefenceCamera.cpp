231694aada1a7c98cf1435888191d936a9c8848c
GuoPuS <gpsvictory@163.com>
2021-05-28 17:17:32 +0800

    Frame

--- Source/TowerDefence/Private/Core/GameCore/TowerDefenceCamera.cpp
+++ Source/TowerDefence/Private/Core/GameCore/TowerDefenceCamera.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "../../Public/GameCore/TowerDefenceCamera.h"
+#include "Core/GameCore/TowerDefenceCamera.h"
 
 // Sets default values
 ATowerDefenceCamera::ATowerDefenceCamera()
